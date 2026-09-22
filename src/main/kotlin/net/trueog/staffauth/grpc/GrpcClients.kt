8a93f36885f59764df3101fc6c8c17c5749c092a
Sven <skbotnl@disroot.org>
2026-09-22 18:13:28 +0200

    gRPC SSL/TLS support

--- src/main/kotlin/net/trueog/staffauth/grpc/GrpcClients.kt
+++ src/main/kotlin/net/trueog/staffauth/grpc/GrpcClients.kt
@@ -1,14 +1,35 @@
 package net.trueog.staffauth.grpc
 
 import io.grpc.ManagedChannel
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
 import io.micronaut.context.annotation.Factory
-import io.micronaut.grpc.annotation.GrpcChannel
+import jakarta.inject.Named
 import jakarta.inject.Singleton
+import net.trueog.staffauth.configuration.PluginConfiguration
 import proto.IpCheckerGrpcKt
+import java.io.File
 
 @Factory
 class GrpcClients {
     @Singleton
-    fun ipCheckStub(@GrpcChannel($$"${plugin.address}") channel: ManagedChannel) =
+    @Named("ipCheckChannel")
+    fun ipCheckChannel(config: PluginConfiguration) : ManagedChannel {
+        val builder = NettyChannelBuilder.forAddress(config.host, config.port)
+        if (config.ssl.enabled) {
+            builder.sslContext(
+                GrpcSslContexts.forClient()
+                    .trustManager(File(config.ssl.trustCa))
+                    .keyManager(File(config.ssl.cert), File(config.ssl.privateKey))
+                    .build()
+            )
+        } else {
+            builder.usePlaintext()
+        }
+        return builder.build()
+    }
+
+    @Singleton
+    fun ipCheckStub(@Named("ipCheckChannel") channel: ManagedChannel) =
         IpCheckerGrpcKt.IpCheckerCoroutineStub(channel)
 }
